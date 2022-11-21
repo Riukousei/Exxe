5ebc647324b8e8e67b9081dd1edb6f83e0b03317
RicardoEdsson <ricardo_edsson@outlook.es>
2022-11-21 04:38:17 -0600

    MenuInicial-MenuPausa
    Agregue el menú principal (Nueva escena) en donde podemos elegir la opción de jugar, opciones, salir.
    En el menú de opciones lo encontramos como botón al estar en la partida, ahí podremos reanudar, reiniciar y salir del juego.
    NOTA: Falta terminar el apartado de opciones y poder salir desde la partida.

---
+++ Assets/Scripts/ScriptsUI/BotonCanvas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonCanvas : MonoBehaviour
+{
+    public void Menu()
+    {
+        Debug.Log("Ir a Menu");
+    }
+}
