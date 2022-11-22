27f5d00ad03b12d2d26cf350d41d7a69be421f7d
Riukousei <68568769+Riukousei@users.noreply.github.com>
2022-11-22 16:04:24 -0600

    Golpe Nokia
    Se añadió la funcionalidad del golpe de nokia Martillo junto con su animator

--- Assets/Scripts/CombateCaC.cs
+++ Assets/Scripts/CombateCaC.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CombateCaC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform controladorGolpe;
+    [SerializeField] private float radioGolpe;
+    [SerializeField] private float dañoGolpe;
+    [SerializeField] private float CooldownAtaque;
+    [SerializeField] private float cooldownTimer;
+    private Animator animator;
+    [SerializeField] private GameObject dañoArea;
+    [SerializeField] private Transform golpearPiso;
+    private void Start()
     {
+        animator= GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(CooldownAtaque > 0)
+        {
+            CooldownAtaque-=Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.X) && CooldownAtaque <=0)
+        {
+            Golpe();
+            CooldownAtaque = cooldownTimer;
+        }
+            
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Golpe()
     {
-        
+        animator.SetTrigger("Golpe");
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
+        foreach(Collider2D colisionador in objetos)
+        {
+            if (colisionador.CompareTag("Enemy"))
+            {
+                colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
+
+    private void GolpePiso()
+    {
+        Instantiate(dañoArea, golpearPiso.position, golpearPiso.rotation);
     }
 }
